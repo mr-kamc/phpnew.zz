ba6e91bbfdc30313fbd0e289976ed55173bc4685
mr-kamc <mr.kamc@gmail.com>
2016-02-29 16:21:36 +0300

    +View

--- index.php
+++ index.php
@@ -2,10 +2,18 @@
 
 require __DIR__ . '/autoload.php';
 
+$view = new \App\View();
+$view->users = \App\Models\User::findAll();
+var_dump($view->users);
+die;
+
+$view->display(__DIR__ . '/App/templates/index.php');
+
+/*
 $news = \App\Models\News::findLastNews(3);
 
 include __DIR__ . '/App/templates/news.php';
-
+*/
 /*
 $ctrl = !empty($_GET['ctrl'])? 'App\\Controllers\\' . $_GET['ctrl'] : 'App\\Controllers\\News';
 $act = !empty($_GET['act'])? $_GET['act'] : 'Index';
