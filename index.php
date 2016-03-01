bde2eef936174b62041d05a2c9233ff0ae2a9d28
alex <steam-punker@mail.ru>
2016-03-01 19:24:49 +0300

    +mod rewrite

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 require __DIR__ . '/autoload.php';
 
 $controller = new \App\Controllers\News();
+
+$url = $_SERVER['REQUEST_URI'];
+
 $action = $_GET['action'] ?: 'Index';
 $controller->action($action);
 /*
